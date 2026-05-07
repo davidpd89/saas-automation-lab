6a65f14599c721871a6b234a48b5415f014f2b6d
davidpd89 <211959558+davidpd89@users.noreply.github.com>
2026-05-07 18:44:49 +0200

    Deploy static site to GitHub Pages

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://davidpd89.github.io",
-  base: "/temp-saas-ai-affiliate-site",
-  output: "static",
-  trailingSlash: "always"
-});
